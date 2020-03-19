a488cf7396a67a2c36e0d641dcff793be5485b62
liel00 <62212244+liel00@users.noreply.github.com>
2020-03-19 18:43:05 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import {robots} from './robots';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
 
 
 
@@ -9,10 +10,16 @@ class  App extends Component {
         constructor(){
             super()
             this.state={
-                robots:robots,
+                robots:[],
                  searchfield: '' 
             }
         }
+        componentDidMount(){
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users=> this.setState({robots:users}))  
+        }
+            
 
         onSearchChange = (event) => {
             this.setState({searchfield:event.target.value})
@@ -21,15 +28,17 @@ class  App extends Component {
            
 
     render(){
-        const filteredRobots = this.state.robots.filter( robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const filteredRobots = this.state.robots.filter( robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
 
         return (
         <div className='tc'>
-        <h1> RoboFrinds </h1>
+        <h1 className='f1'> RoboFrinds </h1>
         <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
         <CardList robots={filteredRobots}/>
+        </Scroll>   
         </div>
     );
   } 
