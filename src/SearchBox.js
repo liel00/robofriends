33051a3ecb536b7b359257471eeb6ae565b3f52b
liel00 <62212244+liel00@users.noreply.github.com>
2020-03-19 18:43:43 +0200

    Add files via upload

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SearchBox = ({searhfield,searchChage}) => {
+const SearchBox = ({searchChange}) => {
     return (
         <div className='pa2'>
         <input className='pa3 ba b--green bg-lightest-blue'
          type= 'search'
           placeholder='search robots' 
-          onChange={searchChage}
+          onChange={searchChange}
           />
             
         </div>
